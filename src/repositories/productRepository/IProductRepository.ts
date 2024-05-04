032fb7098fb1c2a75aeea42942b2e3b2b5e6755e
Alessandro-Massarotti-junior <alemassajr@hotmail.com>
2024-05-04 18:10:53 -0300

    feat(product repository): create interfaces
    create interfaces

---
+++ src/repositories/productRepository/IProductRepository.ts
@@ -0,0 +1,19 @@
+import {
+  CreateDTO,
+  CreateReturnDTO,
+  DeleteDTO,
+  DeleteReturnDTO,
+  FindByIdDTO,
+  FindByIdReturnDTO,
+  GetAllReturnDTO,
+  UpdateDTO,
+  UpdateReturnDTO,
+} from './ProductRepositoryDTOs';
+
+export interface IProductRepository {
+  create(data: CreateDTO): CreateReturnDTO;
+  findById(data: FindByIdDTO): FindByIdReturnDTO;
+  update(data: UpdateDTO): UpdateReturnDTO;
+  delete(data: DeleteDTO): DeleteReturnDTO;
+  getAll(): GetAllReturnDTO;
+}
---
+++ src/repositories/productRepository/ProductRepositoryDTOs.ts
@@ -0,0 +1,18 @@
+import { Product } from '../../entities/Product/Product';
+import { Type } from '../../types';
+
+export type CreateDTO = Type.Product;
+export type CreateReturnDTO = Promise<Product>;
+export type UpdateDTO = Partial<Omit<Type.Product, 'id' | 'createdAt' | 'updatedAt'>> & {
+  id: string;
+};
+export type UpdateReturnDTO = Promise<Product>;
+export type FindByIdDTO = {
+  id: string;
+};
+export type FindByIdReturnDTO = Promise<Product | null>;
+export type DeleteDTO = {
+  id: string;
+};
+export type DeleteReturnDTO = Promise<void>;
+export type GetAllReturnDTO = Promise<Product[]>;
