e41ef1bc0db9f2a597e20a13040a2ae8d85a9fcc
Alessandro-Massarotti-junior <alemassajr@hotmail.com>
2024-05-18 12:41:19 -0300

    feat(server): creating express implementation
    creating express implementation

--- src/http/server/interfaces/IServer.ts
+++ src/http/server/interfaces/IServer.ts
@@ -1,6 +1,9 @@
 import { Router } from '../../routes/Router';
+import * as http from 'http';
 
 export interface IServer {
   setRouter(router: Router): void;
   listen(port: number, callback: () => void): void;
+  close(): void;
+  getServerInstance(): http.Server;
 }
